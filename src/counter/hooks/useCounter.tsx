0e4347648a4768ea5a7913079bbb8742477c52ca
Patricio <native.devil@gmail.com>
2025-09-13 11:39:08 -0300

    ADD: Cache on gifs search

---
+++ src/counter/hooks/useCounter.tsx
@@ -0,0 +1,28 @@
+
+import { useState } from 'react';
+
+
+export const useCounter = (initialValue: number = 10) => {
+  const [counter, setCounter] = useState(initialValue);
+
+  const handleAdd = () => {
+    setCounter(prevState => prevState + 1);
+  }
+
+  const handleSubstract = () => {
+    setCounter(prevState => prevState - 1);
+  }
+
+  const handleReset = () => {
+    setCounter(initialValue);
+  }
+
+  return {
+    // Values
+    counter,
+    // Methods
+    handleAdd,
+    handleSubstract,
+    handleReset
+  }
+}
